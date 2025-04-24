d182169278b438d1823d703ad2265f01acee0230
JanodDev <janodpethum@gmail.com>
2025-04-24 20:03:43 +0530

    show genareted interview on the hompage

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -11,7 +11,7 @@
 //       <Agent
 //         userName={user?.name!}
 //         userId={user?.id}
-//         profileImage={user?.profileURL}
+//         // profileImage={user?.profileURL}
 //         type="generate"
 //       />
 //     </>
@@ -20,15 +20,17 @@
 
 // export default Page;
 
-import React from "react";
 import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = () => {
+const Page = async () => {
+  const user = await getCurrentUser();
+
   return (
     <>
       <h3>Interview Generation</h3>
 
-      <Agent userName="You" userId="user1" type="generate" />
+      <Agent userName={user?.name} userId={user?.id} type="generate" />
     </>
   );
 };
